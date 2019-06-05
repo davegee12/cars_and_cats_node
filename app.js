8ef8948f44a983ffffd4bf1fc507d332f61710ae
davegee12 <davidgee@Davids-MacBook-Air.local>
2019-06-05 11:55:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+
+    if(request.url === '/') {
+        fs.readFile('views/index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/cats') {
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/cars/new') {
+        fs.readFile('views/new.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/car-101975_960_720.jpg') {
+        fs.readFile('./images/car-101975_960_720.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/auto-1694365_960_720.jpg') {
+        fs.readFile('./images/auto-1694365_960_720.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/corvette-3864797_960_720.jpg') {
+        fs.readFile('./images/corvette-3864797_960_720.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/animal-1868911_960_720.jpg') {
+        fs.readFile('./images/animal-1868911_960_720.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/cat-4226750_960_720.jpg') {
+        fs.readFile('./images/cat-4226750_960_720.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/images/cat-694730_960_720.jpg') {
+        fs.readFile('./images/cat-694730_960_720.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+
+server.listen(7077);
+
+console.log("Running in localhost at port 7077");
